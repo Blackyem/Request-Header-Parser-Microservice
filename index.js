700d1fc41bdfa7bd748381fbe139d0fb5d2c6bb7
yelpcamp-profile <kolayem44@gmail.com>
2022-10-21 23:12:02 +0100

    update

--- index.js
+++ index.js
@@ -2,14 +2,24 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
 var express = require('express');
+const os = require("node:os")
+const dotenv = require("dotenv")
+
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+const {
+  Agent
+} = require('node:http');
+const {
+  isIPv4
+} = require('node:net');
+app.use(cors({
+  optionsSuccessStatus: 200
+})); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -20,11 +30,20 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+const resObj = {};
+
+app.get("/api/whoami", (req, res) => {
+  console.log(req.ip);
+  resObj["ipaddress"] = (req.socket.remoteAddress)
+  resObj["language"] = req.header("accept-language");
+  resObj["software"] = req.header("user-agent");
+  res.json(resObj);
 });
+
+
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-});
+})
